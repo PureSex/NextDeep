7105e23ed36f9e8782c3bb679cee77b4e646e774
PureSex <1571588080@qq.com>
2025-02-23 10:46:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -66,6 +66,15 @@ const BLOG = {
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
+
+  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
+  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/388561300', // B站主页
+  CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://www.youtube.com/@Freedom-wh5jq', // Youtube主页
+  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/nextdeep', // 你的github个人主页 例如 https://github.com/tangly1024
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'NextDeep@proton.me', // 邮箱地址 例如mail@tangly1024.com
+
+
+
 }
 
 module.exports = BLOG

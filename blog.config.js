af88a973dcb0f3786a0c9eb55767a58ccabed048
PureSex <1571588080@qq.com>
2025-02-23 10:59:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -68,12 +68,15 @@ const BLOG = {
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/388561300', // B站主页
-  CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://www.youtube.com/@Freedom-wh5jq', // Youtube主页
-  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/nextdeep', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'NextDeep@proton.me', // 邮箱地址 例如mail@tangly1024.com
-
-
+  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
+  CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
+  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/nextdeep', // 你的github个人主页 例如 https://github.com/tangly1024
+  CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
+  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
+  CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/388561300', // B站主页
+  CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'www.youtube.com/@Freedom-wh5jq', // Youtube主页
 
 }
 

563369a64a751da416db1a88a5c822aaece54fa8
PureSex <1571588080@qq.com>
2025-02-23 10:49:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NextDeep', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '🍀生命不息 · 折腾不止🍀', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nextdeep.net', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nextdeep.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'NextDeep, 课程', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
